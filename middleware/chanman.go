5dd2119b674448fe8f42ce905fe984372adc7731
deku0110 <yjs921218@sina.com>
2017-11-30 22:58:09 +0800

    通道管理器和池的实现

--- middleware/chanman.go
+++ middleware/chanman.go
@@ -1,6 +1,11 @@
 package middleware
 
-import "webcrawler/base"
+import (
+	"webcrawler/base"
+	"sync"
+	"fmt"
+	"errors"
+)
 
 // 被用来表示通道管理器的状态的类型。
 type ChannelManagerStatus uint8
@@ -17,17 +22,133 @@ var statusNameMap = map[ChannelManagerStatus]string{
 	CHANNEL_MANAGER_STATUS_INITIALIZED:   "initialized",
 	CHANNEL_MANAGER_STATUS_CLOSED:        "closed",
 }
+
 type ChannelManager interface {
 	//reset代表是否重新初始化通道管理器
-	Init(channelArgs base.ChannelArgs,reset bool) bool
-
+	Init(channelArgs base.ChannelArgs, reset bool) bool
 	Close() bool
-	ReqChan() (chan base.Request,error)
-	RespChan() (chan base.Response,error)
-	ItemChan() (chan base.Item,error)
-	ErrorChan() (chan error,error)
+	ReqChan() (chan base.Request, error)
+	RespChan() (chan base.Response, error)
+	ItemChan() (chan base.Item, error)
+	ErrorChan() (chan error, error)
 	//获取通道管理器的状态
 	Status() ChannelManagerStatus
 	//获取摘要信息
 	Summary() string
+}
+
+//通道管理器的实现类型
+type myChannelManager struct {
+	channelArgs base.ChannelArgs //通道的长度值
+	reqCh       chan base.Request
+	respCh      chan base.Response
+	itemCh      chan base.Item
+	errorCh     chan error
+	status      ChannelManagerStatus //通道管理器的状态
+	rwmutex     sync.RWMutex         //读写锁
+}
+
+func NewChannelManager(channelArgs base.ChannelArgs) ChannelManager {
+	chanman := &myChannelManager{}
+	chanman.Init(channelArgs, true)
+	return chanman
+}
+
+func (cm *myChannelManager) Init(channelArgs base.ChannelArgs, reset bool) bool {
+	if err := channelArgs.Check(); err != nil {
+		panic(err)
+	}
+	cm.rwmutex.Lock()
+	defer cm.rwmutex.Unlock()
+	if cm.status == CHANNEL_MANAGER_STATUS_INITIALIZED && !reset {
+		return false
+	}
+	cm.channelArgs = channelArgs
+	cm.reqCh = make(chan base.Request, channelArgs.ReqChanLen())
+	cm.respCh = make(chan base.Response, channelArgs.RespChanLen())
+	cm.itemCh = make(chan base.Item, channelArgs.ItemChanLen())
+	cm.errorCh = make(chan error, channelArgs.ErrorChanLen())
+	cm.status = CHANNEL_MANAGER_STATUS_INITIALIZED
+	return true
+}
+
+func (cm *myChannelManager) Close() bool {
+	cm.rwmutex.Lock()
+	defer cm.rwmutex.Unlock()
+	if cm.status != CHANNEL_MANAGER_STATUS_INITIALIZED {
+		return false
+	}
+	close(cm.reqCh)
+	close(cm.respCh)
+	close(cm.itemCh)
+	close(cm.errorCh)
+	cm.status = CHANNEL_MANAGER_STATUS_CLOSED
+	return true
+}
+
+func (cm *myChannelManager) ReqChan() (chan base.Request, error) {
+	cm.rwmutex.Lock()
+	defer cm.rwmutex.Unlock()
+	if err := cm.checkStatus(); err != nil {
+		return nil, err
+	}
+	return cm.reqCh, nil
+}
+
+func (cm *myChannelManager) RespChan() (chan base.Response, error) {
+	cm.rwmutex.Lock()
+	defer cm.rwmutex.Unlock()
+	if err := cm.checkStatus(); err != nil {
+		return nil, err
+	}
+	return cm.respCh, nil
+}
+
+func (cm *myChannelManager) ItemChan() (chan base.Item, error) {
+	cm.rwmutex.Lock()
+	defer cm.rwmutex.Unlock()
+	if err := cm.checkStatus(); err != nil {
+		return nil, err
+	}
+	return cm.itemCh,nil
+}
+
+func (cm *myChannelManager) ErrorChan() (chan error, error) {
+	cm.rwmutex.Lock()
+	defer cm.rwmutex.Unlock()
+	if err := cm.checkStatus(); err != nil {
+		return nil, err
+	}
+	return cm.errorCh,nil
+}
+
+func (cm *myChannelManager) Status() ChannelManagerStatus {
+	return cm.status
+}
+
+var chanmanSummaryTemplate = "status: %s, " +
+	"requestChannel: %d/%d, " +
+	"responseChannel: %d/%d, " +
+	"itemChannel: %d/%d, " +
+	"errorChannel: %d/%d"
+
+func (chanman *myChannelManager) Summary() string {
+	summary := fmt.Sprintf(chanmanSummaryTemplate,
+		statusNameMap[chanman.status],
+		len(chanman.reqCh), cap(chanman.reqCh),
+		len(chanman.respCh), cap(chanman.respCh),
+		len(chanman.itemCh), cap(chanman.itemCh),
+		len(chanman.errorCh), cap(chanman.errorCh))
+	return summary
+}
+func (cm *myChannelManager) checkStatus() error {
+	if cm.status == CHANNEL_MANAGER_STATUS_INITIALIZED {
+		return nil
+	}
+	statusName, ok := statusNameMap[cm.status]
+	if !ok {
+		statusName = fmt.Sprintf("%d", cm.status)
+	}
+	errMsg := fmt.Sprintf("The undesirable status of channel manager:%s!\n", statusName)
+	return errors.New(errMsg)
 }
--- middleware/pool.go
+++ middleware/pool.go
@@ -1,5 +1,12 @@
 package middleware
 
+import (
+	"reflect"
+	"errors"
+	"fmt"
+	"sync"
+)
+
 type Pool interface {
 	Take() (Entity, error)
 	Return(entity Entity) error
@@ -9,4 +16,94 @@ type Pool interface {
 
 type Entity interface {
 	Id() uint32
+}
+
+type myPool struct {
+	total       uint32
+	etyep       reflect.Type
+	genEntity   func() Entity
+	container   chan Entity
+	idContainer map[uint32]bool //实体ID的容器
+	mutex       sync.Mutex      //互斥锁
+}
+
+func NewPool(total uint32, etype reflect.Type, genEntity func() Entity) (Pool, error) {
+	if total == 0 {
+		errMsg := fmt.Sprintf("The pool can not be initialized! (total=%d)\n", total)
+		return nil, errors.New(errMsg)
+	}
+	size := int(total)
+	idContainer := make(map[uint32]bool)
+	container := make(chan Entity, size)
+	for i := 0; i < size; i++ {
+		newEntity := genEntity()
+		if etype != reflect.TypeOf(newEntity) {
+			errMsg := fmt.Sprintf("The type of result of function genEntity() is not %s!\n", etype)
+			return nil, errors.New(errMsg)
+		}
+		container <- newEntity
+		idContainer[newEntity.Id()] = true
+	}
+	pool := &myPool{
+		total:       total,
+		genEntity:   genEntity,
+		container:   container,
+		etyep:       etype,
+		idContainer: idContainer}
+	return pool, nil
+}
+
+func (pool *myPool) Take() (Entity, error) {
+	entity, ok := <-pool.container
+	if !ok {
+		return nil, errors.New("The inner container is invalid")
+	}
+	pool.mutex.Lock()
+	defer pool.mutex.Unlock()
+	pool.idContainer[entity.Id()] = false
+	return entity, nil
+}
+
+func (pool *myPool) Return(entity Entity) error {
+	if entity == nil {
+		return errors.New("the returnning entity is invalid!")
+	}
+	if pool.etyep != reflect.TypeOf(entity) {
+		errMsg := fmt.Sprintf("The type of returning entity is NOT %s!\n", pool.etyep)
+		return errors.New(errMsg)
+	}
+	entityId := entity.Id()
+	result := pool.compareAndSetForIdContainer(entityId, false, true)
+	if result == -1 {
+		errMsg := fmt.Sprintf("The entity (id=%d) is invalid!\n", entityId)
+		return errors.New(errMsg)
+	}
+	if result == 0 {
+		errMsg := fmt.Sprintf("The entity (id=%d) is already in the pool !\n", entityId)
+		return errors.New(errMsg)
+}
+	pool.container <- entity
+	return nil
+}
+//结果值 -1表示键值不存在,0表示失败 1表示成功
+func (pool *myPool) compareAndSetForIdContainer(entityId uint32, oldValue bool, newValue bool) uint8 {
+	pool.mutex.Lock()
+	defer pool.mutex.Unlock()
+	v, ok := pool.idContainer[entityId]
+	if !ok {
+		return -1
+	}
+	if v != oldValue {
+		return 0
+	}
+	pool.idContainer[entityId] = newValue
+	return 1
+}
+
+func (pool *myPool) Total() uint32 {
+	return pool.total
+}
+
+func (pool *myPool) Used() uint32 {
+	return pool.total - uint32(len(pool.container))
 }
